c22150f85eaf9bce9d3b737df3cc071a6892fd64
aina.rovira <49318744-M@epp.urv.cat>
2024-12-21 16:18:57 +0100

    roof

--- MyAdventures/house.py
+++ MyAdventures/house.py
@@ -24,6 +24,28 @@ class BuildDestroy:
             for j in range(height):
                 self.mc.setBlock(x + i, y + j, z, 102)  # id for Glass block
 
+    def build_roof(self, x, y, z, width, depth, height, block_id):
+        for i in range(height): 
+            for j in range(depth):
+                # Left slope
+                self.mc.setBlock(x + i, y + i, z + j, block_id)
+                self.mc.setBlock(x + i - 1, y + i, z + j, block_id)
+                # Right slope
+                self.mc.setBlock(x + width - i - 1, y + i, z + j, block_id)
+                self.mc.setBlock(x + width - i, y + i, z + j, block_id)
+
+        # Add the last line at the top of the roof
+        for j in range(depth):
+            self.mc.setBlock(x + width // 2, y + height - 1, z + j, block_id)
+
+    def build_filler(self, x, y, z, width, height, block_id, direction='z'):
+        for i in range(height):
+            for j in range(width - 2 * i):
+                if direction == 'z':
+                    self.mc.setBlock(x + i + j, y + i, z, block_id)
+                else:  # direction 'x'
+                    self.mc.setBlock(x, y + i, z + i + j, block_id)
+
     def run(self):
         time.sleep(2)
         self.mc.postToChat(f"{self.name} starting!")
@@ -33,25 +55,37 @@ class BuildDestroy:
         offset_x = pos.x + 5
         offset_z = pos.z
 
+        # Dimensions of the house
+        house_width = 11
+        house_depth = 10
+        house_height = 7
+        roof_height = 5
+
         # Build the 4 walls of stone (id: 98)
-        self.build_wall(offset_x, pos.y, offset_z, 11, 7, 98, direction='x')
-        self.build_wall(offset_x, pos.y, offset_z, 10, 7, 98, direction='z') 
-        self.build_wall(offset_x, pos.y, offset_z + 10, 11, 7, 98, direction='x')  
-        self.build_wall(offset_x + 11, pos.y, offset_z, 10, 7, 98, direction='z')  
+        self.build_wall(offset_x, pos.y, offset_z, house_width, house_height, 98, direction='x')
+        self.build_wall(offset_x, pos.y, offset_z, house_depth, house_height, 98, direction='z')
+        self.build_wall(offset_x, pos.y, offset_z + house_depth, house_width, house_height, 98, direction='x')
+        self.build_wall(offset_x + house_width - 1, pos.y, offset_z, house_depth, house_height, 98, direction='z')
 
         # Build columns at the 4 corners in pink (id: 201)
-        self.build_column(offset_x, pos.y, offset_z, 7, 201)
-        self.build_column(offset_x + 11, pos.y, offset_z, 7, 201)
-        self.build_column(offset_x, pos.y, offset_z + 10, 7, 201)
-        self.build_column(offset_x + 11, pos.y, offset_z + 10, 7, 201)
+        self.build_column(offset_x, pos.y, offset_z, house_height, 201)
+        self.build_column(offset_x + house_width - 1, pos.y, offset_z, house_height, 201)
+        self.build_column(offset_x, pos.y, offset_z + house_depth, house_height, 201)
+        self.build_column(offset_x + house_width - 1, pos.y, offset_z + house_depth, house_height, 201)
 
         # Add windows
         self.build_window(offset_x + 2, pos.y + 2, offset_z, 3, 3)
         self.build_window(offset_x + 7, pos.y + 2, offset_z, 3, 3)
-        self.build_window(offset_x + 2, pos.y + 2, offset_z + 10, 3, 3)
-        self.build_window(offset_x + 7, pos.y + 2, offset_z + 10, 3, 3)
+        self.build_window(offset_x + 2, pos.y + 2, offset_z + house_depth, 3, 3)
+        self.build_window(offset_x + 7, pos.y + 2, offset_z + house_depth, 3, 3)
 
-        self.mc.postToChat(f"{self.name} has finished building the house")
+        # Build a filler between the wall and the roof
+        self.build_filler(offset_x, pos.y + house_height, offset_z, house_width, roof_height - 1, 98, direction='z')
+        self.build_filler(offset_x, pos.y + house_height, offset_z + house_depth, house_width, roof_height - 1, 98, direction='z')
+
+        # Build the roof with single-layer wood and a top ridge
+        self.build_roof(offset_x , pos.y + house_height, offset_z - 1, house_width, house_depth + 3, roof_height, 17)  
+
 
 # Code to execute the bot
 if __name__ == "__main__":
@@ -64,4 +98,5 @@ if __name__ == "__main__":
     # Run the bot's actions
     bot.run()
     time.sleep(10)  # Pause for 10 seconds before ending the program
+
 
